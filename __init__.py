e4d8fff9f3a088351ea5773db11274e2117e744c
QAsQ <317661924@qq.com>
2017-03-12 18:59:02 +0800

    multi room support

--- __init__.py
+++ __init__.py
@@ -1,51 +1,71 @@
 from flask import Flask, render_template,g,request,redirect
-from flask_socketio import SocketIO,send,emit
-#from flaskext.lon import lon_user,lout_user,current_user ,lon_required
+from flask_socketio import SocketIO,send,emit,join_room,leave_room
+from flask_login import login_user,logout_user,current_user ,login_required,UserMixin,login_manager,LoginManager
 
 app = Flask(__name__)
 socketio = SocketIO(app)
-class roomState:
-    def __init__(self):
-        self.v = 0;
 
-    def tryin(self,x):
-        if (self.v>>x) & 1:
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+app.secret_key = 'OrzQAQ'
+class User(UserMixin):
+    def __init__(self,id):
+        self.id = id;
+
+    def get_id(self):
+        return self.id;
+
+
+class Rooms():
+    def __init__(self):
+        self.roomInfo = dict();
+
+    def state(self,room):
+        if (room in self.roomInfo) == False:
+            self.roomInfo[room] = 0;
+        return self.roomInfo[room];
+
+    def tryJoinRoom(self,room,x):
+        v = self.state(room);
+        if (v>>x) & 1:
             return False;
         else:
-            self.v |= 1<<x;
-            print self.v;
+            self.roomInfo[room] |= 1<<x;
             return True;
 
-rom = roomState();
+rooms= Rooms();
+
+@login_manager.user_loader
+def load_user(userid):
+    return  User(userid);
 
 @socketio.on('battle')
 def handle_battle(Sta):
-    emit('battle',Sta,broadcast=True);
+    room = current_user.id.split(' ')[0];
+    emit('battle',Sta,room = room);
 
 @socketio.on('login')
-def login(ver):
-    emit('romsta',{"o":rom.v},broadcast=True);
+def login(val):
+    room = current_user.id.split(' ')[0];
+    join_room(room);
+    emit('romsta',{"o":rooms.state(room)},room=room);
 
 
 
-@app.route("/ask/<ind>")
-def handle_query(ind):
-    id = int(ind);
-    print ind;
-    if rom.tryin(id):
-        return render_template("playGround.html",play = id)
+@app.route("/<room>/play/<_ind>")
+def handle_query(room,_ind):
+    ind = int(_ind);
+    if rooms.tryJoinRoom(room,ind):
+        login_user(User(room+" "+_ind));
+        return render_template("playGround.html",play = ind)
     else:
-        return redirect("/home");
+        return redirect("/%s" % (room,));
 
-@app.route("/home")
-def index():
-    return render_template('index.html',sta=rom.v);
 
-@app.route("/clear")
-def clear():
-    rom.v = 0;
-    return "Succ";
-
+@app.route("/<room>")
+def roomIndex(room):
+    return render_template('index.html',sta=str(rooms.state(room)),room=room);
 
 if __name__ == '__main__':
-    socketio.run(app,host='0.0.0.0',port=80,debug=True);
+    socketio.run(app,host='0.0.0.0',port=80);
