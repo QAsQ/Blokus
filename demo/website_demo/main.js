0a3433fb619cf9f60a3a9fe3f0fa116c2b995dd9
QAsQ <317661924@qq.com>
2017-12-02 01:02:44 +0800

    [2.0]::refact demo, chess factory added

--- demo/website_demo/main.js
+++ demo/website_demo/main.js
@@ -1,78 +1,84 @@
 /**
  * Created by QAQ on 2017/11/30.
  */
+gCellWidth = 50;
+gCellHeight = 50;
 
-function generate_texture(color, app) {
-    var graphics = new PIXI.Graphics();
-
-    graphics.beginFill(color, 1);
-    graphics.drawRect(0, 0, 50, 50);
-
-    return graphics.generateTexture();
-}
-function onDragStart(event) {
-    this.data = event.data;
-    this.alpha = 0.5;
-    this.dragging = true;
-    var v = event.data.getLocalPosition(this);
-    var anchor = event.currentTarget.anchor;
-    console.log(this.tag);
-    console.log(event.currentTarget.tag)
-    anchor.x += v.x / 50;
-    anchor.y += v.y / 50;
-    this.x += v.x;
-    this.y += v.y;
-}
-
-function onDragEnd() {
-    this.alpha = 1;
-    this.dragging = false;
-    this.data = null;
-}
-
-function onDragMove() {
-    if (this.dragging) {
-        var new_position = this.data.getLocalPosition(this.parent);
-        this.x = new_position.x;
-        this.y = new_position.y;
+/**
+ *  这是什么：一个棋子
+ *  具体：一个 sprite 对象 + 一些方法
+ *  Factory：(app, color, cell_list)
+ *  怎么用：
+ *  什么方法
+ *      翻转，旋转
+ *      获得顶点的 Point
+ *
+ *      更新 anchor & hitArea(by offset)
+ *
+ */
+function ChessFactory(app, cell_list, chess_color) {
+    function onDragStart(event) {
+        this.data = event.data;
+        this.alpha = 0.5;
+        this.dragging = true;
+        var v = event.data.getLocalPosition(this);
+        var anchor = event.currentTarget.anchor;
+        anchor.x += v.x / 100;
+        anchor.y += v.y / 100;
+        this.x += v.x;
+        this.y += v.y;
     }
-}
 
-function generate_item(app, x, y) {
-    var texture = generate_texture(0xFFF122, app);
-    var item = new PIXI.Sprite(texture);
-    item.x = x;
-    item.y = y;
-    return item;
-}
+    function onDragEnd() {
+        this.alpha = 1;
+        this.dragging = false;
+        this.data = null;
+    }
 
-function seter(group) {
+    function onDragMove() {
+        if (this.dragging) {
+            var new_position = this.data.getLocalPosition(this.parent);
+            this.x = new_position.x;
+            this.y = new_position.y;
+        }
+    }
+    var vertex_list = [];
+    cell_list.forEach(function (point) {
+        [[0, 0], [0, 1], [1, 1], [1, 0], [0 ,0]].forEach(function (offset) {
+            vertex_list.push(
+                new PIXI.Point(
+                    (point.x + offset[0]) * gCellWidth,
+                    (point.y + offset[1]) * gCellHeight
+                )
+            )
+        })
+    });
+    var graphics = new PIXI.Graphics();
+    graphics.beginFill(chess_color, 1);
 
-}
+    var polygon = new PIXI.Polygon(vertex_list);
+    graphics.drawPolygon(polygon);
 
-function generate_group(app) {
-    var item0 = generate_item(app, 0, 0);
-    var item1 = generate_item(app, 50, 50);
-    //var group = new PIXI.Sprite();
-    var group = generate_item(app, 25, 25);
+    var chess = new PIXI.Sprite(graphics.generateTexture());
+    chess.shape = polygon;
+    chess.interactive = true;
+    chess.buttonMode = true;
+    chess.hitArea = chess.shape;
+    chess.on('pointerdown', onDragStart)
+        .on('pointerup', onDragEnd)
+        .on('pointerupoutside', onDragEnd)
+        .on('pointermove', onDragMove);
 
-    group.addChild(item0);
-    group.addChild(item1);
-    group.interactive = true;
-    group.buttonMode = true;
-    group.anchor.set(0.5);
-    group.tag = "wqa";
-    group.on('pointerdown', onDragStart)
-         .on('pointerup', onDragEnd)
-         .on('pointerupoutside', onDragEnd)
-         .on('pointermove', onDragMove);
-    group.x = 100;
-    group.y = 100;
-
-    return group;
+    return chess;
 }
 
 var app = new PIXI.Application(800, 600, { backgroundColor: 0x1099bb});
 document.body.appendChild(app.view);
 
-app.stage.addChild(generate_group(app));
+cell_list = [
+    new PIXI.Point(0, 0),
+    new PIXI.Point(1, 1)
+]
+var chess = ChessFactory(app, cell_list, 0xFFBB00);
+app.stage.addChild(chess);
+
