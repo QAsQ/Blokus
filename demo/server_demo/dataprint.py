f75e8e42b63a46a6b1553f83b3f1b0a495ec9ce2
igoodvegetablea <lzy960601@outlook.com>
2017-12-06 13:10:53 +0800

    check piece_gen && lower_right_limit_gen

--- demo/server_demo/dataprint.py
+++ demo/server_demo/dataprint.py
@@ -1,4 +1,4 @@
-from data import Point, PieceShape, adhoc_piece_shape_set_generate
+from data import Point, PieceShape, adhoc_piece_shape_set_generate, lower_right_limit_generate
 
 def pri(matrix):
     for i in range(5):
@@ -9,11 +9,13 @@ def pri(matrix):
 
 def main():
     Pieces = adhoc_piece_shape_set_generate()
+    lower_right_limit = lower_right_limit_generate()
     for piece_id in range(21):
         for state in range(8):
             Position_Matrix = [['.' for j in range(5)] for i in range(5)]
             for point in Pieces[piece_id][state]:
                 Position_Matrix[point.x][point.y]='*'
+            print(lower_right_limit[piece_id][state].x, ' ', lower_right_limit[piece_id][state].y)
             pri(Position_Matrix)
 
 if __name__ == '__main__':
