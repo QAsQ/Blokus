9693690813f22843aae30e38a79cce3b3474e973
igoodvegetablea <lzy960601@outlook.com>
2017-12-09 01:05:32 +0800

    no compile error!

--- app/models/piece.py
+++ app/models/piece.py
@@ -6,16 +6,17 @@ class Position:
 
     def from_dict(self, data):
         self.state = data["state"]
-        self.x = data["x"] 
+        self.x = data["x"]
         self.y = data["y"]
         return self
-    
+
     def to_dict(self):
         return {
             "state": self.state,
             "x": self.x,
             "y": self.y
         }
+
 
 def can_place(piece_set, coordinate_x, coordinate_y):
     for piece_point in piece_set:
@@ -52,12 +53,37 @@ def generate_piece_initialize_legal_position(piece_shape, player_id):
             begin_position[i][j] = can_be_placed
     return begin_position
 
+# 0 - share_corner
+# 1 - share_edge
+# 2 - occupy
+def action_generate(position):
+    dir_point = [(-1, -1), (1, -1), (1, 1), (-1, 1), (-1, 0), (1, 0), (0, 1), (0, -1)]
+    result_action = [[[[], [], []] for j in range(20)] for i in range(20)]
+    for i in range(20):
+        for j in range(20):
+            if check_legal_posi(i, j, position) == False:
+                continue
+            for pos in position:
+                now_pos = (pos[0] + i, pos[1] + j)
+                for k in range(4):
+                    temp_pos = (now_pos[0] + dir_point[k][0], now_pos[1] + dir_point[k][1])
+                    if legal(temp_pos):
+                        result_action[temp_pos[0]][temp_pos[1]][0].append((i, j))
+                for k in range(4, 8):
+                    temp_pos = (now_pos[0] + dir_point[k][0], now_pos[1] + dir_point[k][1])
+                    if legal(temp_pos):
+                        result_action[temp_pos[0]][temp_pos[1]][1].append((i, j))
+                result_action[now_pos[0]][now_pos[1]][2].append((i, j))
+    return result_action
+
+
 def occupied(act_pos, ano_pos):
     for actp in act_pos:
         for anop in ano_pos:
             if same_point(actp, anop):
                 return 1
     return 0
+
 
 def share_edge(act_pos, ano_pos):
     if same_point((act_pos[0] + 1, act_pos[1]), ano_pos):
@@ -81,6 +107,7 @@ def share_edge(act_pos, ano_pos):
     #             return 1
     # return 0
 
+
 def share_corner(act_pos, ano_pos):
     if same_point((act_pos[0] + 1, act_pos[1] + 1), ano_pos):
         return True
@@ -99,22 +126,24 @@ def share_corner(act_pos, ano_pos):
     #                 return 1
     # return 0
 
-    def legal(posi):
-        if posi[0]>=20 or posi[1]>=20:
-            return False
-        else:
-            return True
-
-    def check_legal_posi(cor_x, cor_y, position):
-        for point in position:
-            if legal(cor_x + point[0], cor_y + point[1]):
-                return False
+def legal(posi):
+    if posi[0] >= 20 or posi[1] >= 20:
+        return False
+    else:
         return True
+
+def check_legal_posi(cor_x, cor_y, position):
+    for point in position:
+        if legal((cor_x + point[0], cor_y + point[1])):
+            return False
+    return True
+
 
 class Piece:
     def __init__(self, piece_shape_set, player_id):
         self.shape_set = piece_shape_set
         self.possible_position = []
+        self.action = []
 
         for state in range(8):
             self.possible_position.append(
@@ -146,30 +175,6 @@ class Piece:
         if 0 > position.y or 20 < position.y:
             return False
         return self.possible_position[position.state][position.x][position.y] == 1
-    
-    # 0 - share_corner
-    # 1 - share_edge
-    # 2 - occupy
-    def action_generate(position):
-        dir_point = [(-1, -1), (1, -1), (1, 1), (-1, 1), (-1, 0), (1, 0), (0, 1), (0, -1)]
-        result_action = [ [ [ [], [], [] ] for j in range(20) ] for i range(20) ]
-        cor_mark = [ [ 0 for j in range(20) ] for i range(20) ]
-        for i in range(20):
-            for j in range(20):
-                if check_legel_bg(i, j, position) == False:
-                    continue
-                for pos in position:
-                    now_pos = (pos[0] + i, pos[1] + j)
-                    for k in range(4):
-                        temp_pos = (now_pos[0] + dir_point[k][0], now_pos[1] + dir_point[k][1])
-                        if legal(temp_pos):
-                            result_action[temp_pos[0]][temp_pos[1]][0].append((i, j))
-                    for k in range(4,8):
-                        temp_pos = (now_pos[0] + dir_point[k][0], now_pos[1] + dir_point[k][1])
-                        if legal(temp_pos):
-                            result_action[temp_pos[0]][temp_pos[1]][1].append((i, j))
-                    result_action[now_pos[0]][now_pos[1]][2].append((i, j))
-        return result_action
 
     def get_one_possible_position(self):
         if self.is_drop:
@@ -186,17 +191,20 @@ class Piece:
     # 2-occupied or share edge with same color or illegal or out of board
     def update_possible_position(self, piece_shape, position, is_same_player):
         for state in range(8):
-            for pos in position:
-                for act in self.result_action[state][pos[0]][pos[1]][2]:
+            for temp_pos in piece_shape:
+                pos = (temp_pos[0] + position[0], temp_pos[1] + position[1])
+                for act in self.action[state][pos[0]][pos[1]][2]:
                     self.possible_position[state][act[0]][act[1]] = 2
             if is_same_player == True:
-                for pos in position:
-                    for act in self.result_action[state][pos[0]][pos[1]][1]:
-                    self.possible_position[state][act[0]][act[1]] = 2
-                for pos in position:
-                    for act in self.result_action[state][pos[0]][pos[1]][0]:
-                    if self.possible_position[state][act[0]][act[1]] == 0:
-                        self.possible_position[state][act[0]][act[1]] = 1
+                for temp_pos in piece_shape:
+                    pos = (temp_pos[0] + position[0], temp_pos[1] + position[1])
+                    for act in self.action[state][pos[0]][pos[1]][1]:
+                        self.possible_position[state][act[0]][act[1]] = 2
+                for temp_pos in piece_shape:
+                    pos = (temp_pos[0] + position[0], temp_pos[1] + position[1])
+                    for act in self.action[state][pos[0]][pos[1]][0]:
+                        if self.possible_position[state][act[0]][act[1]] == 0:
+                            self.possible_position[state][act[0]][act[1]] = 1
         # another_position = []
         # for pos in piece_shape:
         #     another_position.append(
