1f97743ac78789938582abe2ff47f90ebba23bbd
FlierKing <28241863+FlierKing@users.noreply.github.com>
2018-04-04 01:12:53 +0800

    Update rating.py
    使用了理论上更合理的计分方式

--- app/models/rating.py
+++ app/models/rating.py
@@ -2,19 +2,20 @@ import math
 import json
 
 class User(object):
-    def __init__(self, rank, old_rating, index = -1):
+    def __init__(self, rank, old_rating, left, index = -1):
         self.index = index,
         self.rank = float(rank)
+        self.left = int(left)
         self.old_rating = int(old_rating)
 
 class RatingCalculator(object):
     def __init__(self, users):
         self.user_list = []
         for user in users:
-            self.user_list.append(User(user['rank'], user['rating'], user['index']))
+            self.user_list.append(User(user['rank'], user['rating'], user['left'], user['index']))
 
     def cal_p(self, user_a, user_b):
-        return 1.0 / (1.0 + pow(10, (user_b.old_rating - user_a.old_rating) / 400.0))
+        return 1.0 / pow(10, (user_b.old_rating - user_a.old_rating) / 400.0)
 
     def get_ex_seed(self, user_list, rating, own_user):
         ex_user = User(0.0, rating)
@@ -36,29 +37,21 @@ class RatingCalculator(object):
         return left
 
     def calculate(self):
-        # Calculate seed
+        # Calculate multi
+        self.user_list = sorted(self.user_list, key=lambda x: x.left)
+        sum_dis = 0
         for i in range(len(self.user_list)):
-            self.user_list[i].seed = 1.0
-            for j in range(len(self.user_list)):
-                if i != j:
-                    self.user_list[i].seed += self.cal_p(self.user_list[j], self.user_list[i])
-        # Calculate initial delta and sum_delta
-        sum_delta = 0
-        for user in self.user_list:
-            user.delta = int(
-                (self.cal_rating(self.user_list, math.sqrt(user.rank * user.seed), user) - user.old_rating) / 2)
-            sum_delta += user.delta
-        # Calculate first inc
-        inc = int(-sum_delta / len(self.user_list)) - 1
-        for user in self.user_list:
-            user.delta += inc
-        # Calculate second inc
-        self.user_list = sorted(self.user_list, key=lambda x: x.old_rating, reverse=True)
-        s = min(len(self.user_list), int(4 * round(math.sqrt(len(self.user_list)))))
-        sum_s = 0
-        for i in range(s):
-            sum_s += self.user_list[i].delta
-        inc = min(max(int(-sum_s / s), -10), 0)
+            for j in range(i+1,len(self.user_list)):
+                sum_dis += self.user_list[j].left - self.user_list[i].left
+        ave_dis = sum_dis / (len(self.user_list) * (len(self.user_list) - 1) / 2)
+        multi = 4 / ave_dis * len(self.user_list)
+        # Calculate rating changes between every two people
+        for i in range(len(self.user_list)):
+            for j in range(i+1,len(self.user_list)):
+                inc = int(multi * (self.user_list[j].left - self.user_list[i].left) * self.cal_p(self.user_list[j], self.user_list[i]))
+                inc = min(max(inc, 10), 100)
+                self.user_list[i].delta += inc
+                self.user_list[j].delta -= inc
         # Calculate new rating
         for user in self.user_list:
             user.delta += inc
