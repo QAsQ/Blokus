482e57958e982b95eff4bdc135f79ee9b751bcbd
MeiK <meik2333@gmail.com>
2018-04-08 16:36:25 +0800

    添加装饰器 field_checker_json

--- app/models/app_utility.py
+++ app/models/app_utility.py
@@ -1,6 +1,23 @@
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask import jsonify, current_app
 import time
+from functools import wraps
+
+from flask import current_app, g, jsonify, redirect, request, url_for
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+
+
+def field_checker_json(check_list, method=None):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if method == request.method or method is None:
+                request_json = request.get_json(force=True)
+                check_res = field_checker(request_json, check_list)
+                if check_res is not None:
+                    return failure(check_res)
+            return f(*args, **kwargs)
+        return decorated_function
+    return decorator
+
 
 def success(result):
     if result == "":
@@ -13,10 +30,12 @@ def success(result):
             "result": result
         })
 
+
 def failure(messsage):
     return jsonify({
         "message": messsage
     })
+
 
 def field_checker(request_json, fields):
     for field in fields:
@@ -24,8 +43,10 @@ def field_checker(request_json, fields):
             return "{} field not exist".format(field)
     return None
 
+
 def current_time():
     return int(time.time())
+
 
 def require_format(request_args):
     fields = {
@@ -40,12 +61,14 @@ def require_format(request_args):
         value = request_args.get(field, type=fields[field], default=None)
         if value is not None:
             require[field] = value
-    
+
     return require
+
 
 def generate_register_token(email, expiration=3600):
     s = Serializer(current_app.config['SECRET_KEY'], expiration)
     return s.dumps({'email': email}).decode()
+
 
 def get_email_from_token(token):
     s = Serializer(current_app.config['SECRET_KEY'])
@@ -55,11 +78,12 @@ def get_email_from_token(token):
             return False
     except:
         return False
-    
+
     return data.get("email")
+
 
 def token_verify(db, token):
     if list(db.token.find({"token": token})) == []:
         db.token.insert({"token": token})
         return True
-    return False 
+    return False
--- app/run.py
+++ app/run.py
@@ -9,14 +9,15 @@ from flask_login import LoginManager, current_user, login_user, login_required, 
 from models.user import User
 from models.board import BoardFactory
 from models.battle import Battle, BattleFactory
-from models.theme  import ThemeFactory
+from models.theme import ThemeFactory
 from models.db_utility import init_generate, id_clear, history_clear, id_generate, auth_db, filter_condition_generate, sort_condition_generate, username_checker
-from models.app_utility import success, failure, field_checker, current_time, require_format, generate_register_token, get_email_from_token, token_verify
+from models.app_utility import success, failure, field_checker, current_time, require_format, generate_register_token, get_email_from_token, token_verify, field_checker_json
 from models.mail_utility import send_register_mail, send_reset_mail, send_confirm_email
 
 from config import db_config, app_config, email_config, url_head
 
-db = MongoClient(host=db_config['host'], port=db_config['port'])[db_config['db_name']]
+db = MongoClient(host=db_config['host'], port=db_config['port'])[
+    db_config['db_name']]
 auth_db(db, db_config)
 init_generate(db, ["battles", "users"])
 
@@ -29,6 +30,7 @@ login_manager.anonymous_user = User.anonymous_user(db)
 
 next_battle_cache = dict()
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.load_from_id(db, user_id)
@@ -38,9 +40,11 @@ def load_user(user_id):
 def index_page():
     return render_template("Index.html")
 
+
 @app.route("/battles")
 def battles_page():
     return render_template("battles.html")
+
 
 @app.route("/about")
 def about_page():
@@ -48,15 +52,17 @@ def about_page():
     battle_number = db.battles.find().count()
     return render_template("about.html", user_number=user_number, battle_number=battle_number)
 
+
 @app.route("/rank-list")
 def userlist_page():
-    projection = {"password" : False}
+    projection = {"password": False}
     sort = [("user_info.rating", pymongo.DESCENDING)]
     users = id_clear(db.users.find(
         filter={"user_info.number_of_battles": {"$ne": 0}},
         projection=projection,
         sort=sort))
     return render_template("rank_list.html", users=users)
+
 
 @app.route("/users")
 def user_page():
@@ -70,6 +76,7 @@ def user_page():
         return render_template("error.html", message="该用户不存在")
 
     return render_template("user.html", target_user=user)
+
 
 @app.route("/user_setting")
 def user_setting_page():
@@ -85,6 +92,7 @@ def user_setting_page():
 
     return render_template("user_setting.html", target_user=user, updated=False)
 
+
 @app.route("/register")
 def regiester_page():
     try:
@@ -96,8 +104,9 @@ def regiester_page():
             return render_template("error.html", message="网址已过期")
     except Exception:
         return render_template("error.html", message="没有权限")
-    
+
     return render_template("register.html", email=email, token=token)
+
 
 @app.route("/confirm")
 def confirm_page():
@@ -109,11 +118,12 @@ def confirm_page():
         if not token_verify(db, token):
             return render_template("error.html", message="网址已过期")
     except Exception:
-            return render_template("error.html", message="没有权限")
-    
+        return render_template("error.html", message="没有权限")
+
     current_user.update("email", email)
-    
+
     return render_template("user_setting.html", target_user=current_user, updated=True)
+
 
 @app.route("/password_resetter")
 def password_resetter_page():
@@ -127,7 +137,7 @@ def password_resetter_page():
             return render_template("error.html", message="网址已过期")
     except Exception:
         return render_template("error.html", message="没有权限")
-    
+
     return render_template("password_resetter.html", token=token, user_id=user.user_id)
 
 
@@ -137,7 +147,7 @@ def battle_page():
         battle_id = int(request.args.get('battle_id'))
     except Exception as e:
         return render_template("error.html", message=repr(e))
-    
+
     battle = BattleFactory.load_battle(battle_id, db)
 
     if isinstance(battle, str):
@@ -145,17 +155,16 @@ def battle_page():
 
     return render_template("battle.html", battle=battle.get_state(current_time(), current_user.user_id))
 
+
 @app.route("/api/register", methods=['POST'])
+@field_checker_json(['email'])
 def regiester():
     request_json = request.get_json(force=True)
 
-    check_res = field_checker(request_json, ['email'])
-    if check_res is not None:
-        return failure(check_res)
-
     result = send_register_mail(
-        request_json['email'], 
-        url_head + "/register?token=" + generate_register_token(request_json['email']), 
+        request_json['email'],
+        url_head + "/register?token=" +
+        generate_register_token(request_json['email']),
         email_config
     )
 
@@ -163,17 +172,16 @@ def regiester():
         return failure(result)
     return success("")
 
+
 @app.route("/api/password_resetter", methods=['POST'])
+@field_checker_json(['email'])
 def password_resetter():
     request_json = request.get_json(force=True)
 
-    check_res = field_checker(request_json, ['email'])
-    if check_res is not None:
-        return failure(check_res)
-
     result = send_reset_mail(
-        request_json['email'], 
-        url_head + "/password_resetter?token=" + generate_register_token(request_json['email']), 
+        request_json['email'],
+        url_head + "/password_resetter?token=" +
+        generate_register_token(request_json['email']),
         email_config
     )
 
@@ -181,19 +189,20 @@ def password_resetter():
         return failure(result)
     return success("")
 
+
 @app.route("/api/confirm", methods=['POST'])
+@field_checker_json(['email', 'password'])
 def confirm():
     request_json = request.get_json(force=True)
-    check_res = field_checker(request_json, ['email', 'password'])
-    if check_res is not None:
-        return failure(check_res)
+
     if not current_user.check_password(request_json['password']):
         return failure("原密码错误")
 
     result = send_confirm_email(
         current_user.username,
-        request_json['email'], 
-        url_head + "/confirm?token=" + generate_register_token(request_json['email']), 
+        request_json['email'],
+        url_head + "/confirm?token=" +
+        generate_register_token(request_json['email']),
         email_config
     )
 
@@ -201,7 +210,9 @@ def confirm():
         return failure(result)
     return success("")
 
+
 @app.route("/api/users", methods=['GET', 'POST'])
+@field_checker_json(['username', 'email', 'password', 'token'], method='POST')
 def users():
     if request.method == 'GET':
         try:
@@ -210,8 +221,8 @@ def users():
         except Exception as e:
             return failure(repr(e))
 
-        #remove password
-        projection = {"password" : False}
+        # remove password
+        projection = {"password": False}
         return success(id_clear(db.users.find(
             filter=query,
             projection=projection,
@@ -220,20 +231,17 @@ def users():
     elif request.method == 'POST':
         request_json = request.get_json(force=True)
 
-        check_res = field_checker(request_json, ['username', 'email', 'password', 'token'])
-        if check_res is not None:
-            return failure(check_res)
-        
         try:
-            assert(get_email_from_token(request_json['token']) == request_json['email'])
+            assert(get_email_from_token(
+                request_json['token']) == request_json['email'])
         except Exception as e:
             return failure("Permission Denied")
 
         user = User.create(
-            db, 
-            id_generate, 
-            request_json['username'], 
-            request_json['email'], 
+            db,
+            id_generate,
+            request_json['username'],
+            request_json['email'],
             request_json['password']
         )
         if isinstance(user, str):
@@ -242,19 +250,17 @@ def users():
 
         return success(user.dump())
 
+
 @app.route("/api/users/<int:user_id>", methods=['PUT'])
+@field_checker_json(['old_password', 'new_password', 'confirm_password'])
 def user(user_id):
     if current_user.user_id == -1 or current_user.user_id != user_id:
         return failure("perission denied")
     request_json = request.get_json(force=True)
 
-    check_res = field_checker(request_json, ['old_password'])
-    if check_res is not None:
-        return failure(check_res)
-    
     if not current_user.check_password(request_json['old_password']):
         return failure("原密码错误")
-    
+
     legal_field = ['username', 'password']
     for key in list(request_json):
         if key not in legal_field:
@@ -262,16 +268,15 @@ def user(user_id):
 
     for key in request_json:
         current_user.update(key, request_json[key])
-    
+
     return success(current_user.dump())
 
+
 @app.route("/api/users/<int:user_id>/email_password_resetter", methods=['PUT'])
+@field_checker_json(['token', 'password'])
 def user_email_password_resetter(user_id):
     request_json = request.get_json(force=True)
 
-    check_res = field_checker(request_json, ['token', 'password'])
-    if check_res is not None:
-        return failure(check_res)
     try:
         token = request_json["token"]
         email = get_email_from_token(token)
@@ -280,11 +285,12 @@ def user_email_password_resetter(user_id):
             return failure("Permission denied")
     except Exception:
         return failure("Permission denied")
-    
+
     user.update('password', request_json['password'])
     login_user(user)
-    
+
     return success("")
+
 
 @app.route("/api/users/online", methods=['POST', 'DELETE'])
 def login():
@@ -306,13 +312,15 @@ def login():
 
         return success(current_user.dump())
 
+
 @app.route("/api/themes/<string:themeType>/<string:boardType>", methods=['GET'])
 def themes(themeType, boardType):
     theme = ThemeFactory(themeType, boardType)
     if isinstance(theme, str):
         return failure(theme)
     return success(theme)
-        
+
+
 @app.route("/api/boards/<string:boardType>", methods=['GET'])
 def boards(boardType):
     board_data = BoardFactory.getBoardData(boardType)
@@ -320,20 +328,24 @@ def boards(boardType):
         return failure(board_data)
     return success(board_data)
 
+
 @app.route("/api/battles", methods=['GET', 'POST'])
+@field_checker_json(['battle_name', 'accuracy_time', 'additional_time', 'board_type'], method='POST')
 def battles():
     if request.method == 'GET':
         try:
             query = json.loads(request.args.get("query", "{}"))
             sort = json.loads(request.args.get("sort", "[]"))
-            start = json.loads(request.args.get('start', "{'start': 0}"))['start']
-            limit = json.loads(request.args.get('limit', "{'limit': 30}"))['limit']
+            start = json.loads(request.args.get(
+                'start', "{'start': 0}"))['start']
+            limit = json.loads(request.args.get(
+                'limit', "{'limit': 30}"))['limit']
         except:
             return failure("request syntax error! need json string!")
 
         if "username" in query and not username_checker(db, query['username']):
             return failure("user not exist")
-        
+
         mongo_query = filter_condition_generate(query)
         if isinstance(query, str):
             return failure(query)
@@ -341,30 +353,21 @@ def battles():
         mongo_sort = sort_condition_generate(sort)
         if isinstance(sort, str):
             return failure(sort)
-        
+
         current_user.update_perference(
-            "condition", 
+            "condition",
             {"query": query, "sort": sort}
         )
-        
+
         return success({
             "start": start,
             "battle_list": history_clear(db.battles.find(
-            filter=mongo_query,
-            sort=mongo_sort)[start : start + limit])})
+                filter=mongo_query,
+                sort=mongo_sort)[start: start + limit])})
 
     elif request.method == 'POST':
         if current_user.user_id == -1:
             return failure("need login first!")
-
-        request_json = request.get_json(force=True)
-        check_res = field_checker(request_json, [
-            'battle_name', 
-            'accuracy_time', 
-            'additional_time', 
-            'board_type'])
-        if check_res is not None:
-            return failure(check_res)
 
         battle = BattleFactory.create_battle(
             current_time(),
@@ -374,13 +377,15 @@ def battles():
         )
 
         if isinstance(battle, str):
-            return failure(battle) 
+            return failure(battle)
 
         current_user.update_perference("create", request_json)
 
         return success({"id": battle.id})
 
+
 @app.route("/api/battles/<int:battle_id>", methods=['GET', 'POST'])
+@field_checker_json(['player_id', 'piece_id', 'position'], method='POST')
 def battle(battle_id):
     battle = BattleFactory.load_battle(battle_id, db)
     user_id = current_user.user_id
@@ -392,17 +397,12 @@ def battle(battle_id):
         require = require_format(request.args)
         return success(battle.get_state(current_time(), user_id, require))
 
-
     elif request.method == 'POST':
-        #todo check user_id match player_id
+        # todo check user_id match player_id
         request_json = request.get_json(force=True)
 
-        check_res = field_checker(request_json, ['player_id', 'piece_id', 'position'])
-        if check_res is not None:
-            return failure(check_res)
-
         battle.try_drop_piece(
-            current_time(), 
+            current_time(),
             request_json['player_id'],
             current_user.user_id,
             request_json['piece_id'],
@@ -410,34 +410,35 @@ def battle(battle_id):
         )
         return success(battle.get_state(current_time(), request_json['player_id']))
 
+
 @app.route("/api/battles/<int:battle_id>/next_battle", methods=['GET'])
 def next_battle(battle_id):
     battle = BattleFactory.load_battle(battle_id, db)
 
     if isinstance(battle, str):
         return failure(battle)
-    
+
     if battle_id not in next_battle_cache:
         battle_data = battle.get_state()
-        next_battle = BattleFactory.clone_battle(current_time(), battle_data, db)
+        next_battle = BattleFactory.clone_battle(
+            current_time(), battle_data, db)
         next_battle_cache[battle_id] = next_battle.id
 
     return success({"next_battle": next_battle_cache[battle_id]})
 
+
 @app.route("/api/battles/<int:battle_id>/chat_logs", methods=['POST'])
+@field_checker_json(['content'])
 def chat_logs(battle_id):
     if current_user.user_id == -1:
         return failure("need login first!")
 
     request_json = request.get_json(force=True)
-    check_res = field_checker(request_json, ['content'])
-    if check_res is not None:
-        return failure(check_res)
 
     battle = BattleFactory.load_battle(battle_id, db)
     if isinstance(battle, str):
         return failure(battle)
-    
+
     require = request_json.get("require", {})
     result = battle.append_chat_log(
         current_time(),
@@ -447,8 +448,9 @@ def chat_logs(battle_id):
     )
     if isinstance(result, str):
         return failure(result)
-    
+
     return success(result)
+
 
 @app.route("/api/battles/<int:battle_id>/players/<int:player_id>", methods=['POST', 'DELETE'])
 def players(battle_id, player_id):
@@ -460,17 +462,19 @@ def players(battle_id, player_id):
         return failure(battle)
 
     if request.method == 'POST':
-        result = battle.try_join_player(current_time(), player_id, current_user.user_id, current_user.dump())
+        result = battle.try_join_player(
+            current_time(), player_id, current_user.user_id, current_user.dump())
         if (isinstance(result, str)):
             return failure(result)
-        
+
         return success(result)
-    
+
     elif request.method == 'DELETE':
-        result = battle.try_remove_player(current_time(), player_id, current_user.user_id)
+        result = battle.try_remove_player(
+            current_time(), player_id, current_user.user_id)
         if (isinstance(result, str)):
             return failure(result)
-        
+
         return success(result)
 
 
@@ -485,18 +489,21 @@ def hosting(battle_id, player_id):
         return failure("请先登录!")
 
     if request.method == 'POST':
-        result = battle.add_hosting(current_time(), player_id, current_user.user_id)
+        result = battle.add_hosting(
+            current_time(), player_id, current_user.user_id)
         if (isinstance(result, str)):
             return failure(result)
-        
+
         return success(result)
 
     elif request.method == 'DELETE':
-        result = battle.remove_hosting(current_time(), player_id, current_user.user_id)
+        result = battle.remove_hosting(
+            current_time(), player_id, current_user.user_id)
         if (isinstance(result, str)):
             return failure(result)
-        
+
         return success(result)
+
 
 if __name__ == '__main__':
     #app.run(host='0.0.0.0', port=80, debug=True)
